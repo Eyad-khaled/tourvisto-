2ebb052fec9cdb7596c5c6d8d8c1c70238f6aab1
unknown <ekmelgendy@gmail.com>
2025-12-28 15:01:32 +0200

    gemini-flash

--- server.ts
+++ server.ts
@@ -95,7 +95,7 @@ app.post("/api/create-trip", async (req: Request, res: Response) => {
     ]
     }`;
 
-    const model = genAi.getGenerativeModel({ model: "gemini-3-flash" });
+    const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash" });
     const textResult = await model.generateContent([prompt]);
 
     const trip = parseMarkdownToJson(textResult.response.text());
